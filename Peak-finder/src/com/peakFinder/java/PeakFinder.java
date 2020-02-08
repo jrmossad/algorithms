85265d613fa58711f54bbfa2c652f48fd0e61415
Mahmoud Mossad <mahmoud.mossad@student.guc.edu.eg>
2020-02-08 21:59:47 +0200

    add peak finder enhancement algorithm

--- Peak-finder/src/com/peakFinder/java/PeakFinder.java
+++ Peak-finder/src/com/peakFinder/java/PeakFinder.java
@@ -2,63 +2,73 @@ package com.peakFinder.java;
 
 public class PeakFinder {
 
-	public static void main(String[] args) {
-		int[][] x = { { 10, 8, 10, 10 }, { 14, 13, 12, 11 }, { 15, 9, 11, 21 }, { 16, 17, 19, 20 } };
-		System.out.println(findPeak(x));
+    public static void main(String[] args) {
+        int[][] x = {{4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2},
+                {5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3},
+                {6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4},
+                {7, 8, 9, 10, 11, 10, 9, 8, 7, 6, 5},
+                {8, 9, 10, 11, 12, 11, 10, 9, 8, 7, 6},
+                {7, 8, 9, 10, 11, 10, 9, 8, 7, 6, 5},
+                {6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4},
+                {5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3},
+                {4, 5, 6, 7, 8, 7, 6, 5, 4, 3, 2},
+                {3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1},
+                {2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 0}};
+        System.out.println(findPeak(x));
 
-	}
+    }
 
-	public static int findPeak(int[][] arr) {
-		return findPeakHelper(0, arr[0].length, arr);
-	}
+    public static int findPeak(int[][] arr) {
+        return findPeakHelper(0, arr[0].length, arr);
+    }
 
-	public static int findPeakHelper(int startColumn, int endColumn, int[][] arr) {
-		int middle = (startColumn + endColumn) / 2;
-		int maximum = findMaximum(middle, arr);
+    public static int findPeakHelper(int startColumn, int endColumn, int[][] arr) {
+        int middle = (startColumn + endColumn) / 2;
+        int maximum = findMaximum(middle, arr);
 
-		if (!(isPeak(maximum, middle, arr))) {
-			int bestNeigbour = getBestNeigbour(maximum, middle, arr);
-			int newStartColumn = bestNeigbour > middle ? bestNeigbour : startColumn;
-			int newEndColumn = bestNeigbour > middle ? endColumn : bestNeigbour;
-			if (newEndColumn < newStartColumn || newStartColumn == -1 || newEndColumn == -1) {
-				return -1;
-			}
-			return findPeakHelper(newStartColumn, newEndColumn, arr);
-		}
+        if (!(isPeak(maximum, middle, arr))) {
+            int bestNeighbour = getBestNeighbour(maximum, middle, arr);
+            int newStartColumn = bestNeighbour > middle ? bestNeighbour : startColumn;
+            int newEndColumn = bestNeighbour > middle ? endColumn : bestNeighbour;
+            if (newEndColumn < newStartColumn || newStartColumn == -1 || newEndColumn == -1) {
+                return -1;
+            }
+            return findPeakHelper(newStartColumn, newEndColumn, arr);
+        }
 
-		return arr[maximum][middle];
-	}
+        return arr[maximum][middle];
+    }
 
-	public static int findMaximum(int column, int[][] arr) {
-		int maximum = 0;
+    public static int findMaximum(int column, int[][] arr) {
+        int maximum = 0;
 
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[maximum][column] < arr[i][column]) {
-				maximum = i;
-			}
-		}
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[maximum][column] < arr[i][column]) {
+                maximum = i;
+            }
+        }
 
-		return maximum;
-	}
+        return maximum;
+    }
 
-	public static boolean isPeak(int i, int j, int[][] arr) {
-		int element = arr[i][j];
-		boolean right = ((j < arr[i].length - 1) && (element >= arr[i][j + 1])) || (j == arr[i].length - 1);
-		boolean left = ((j > 0) && (element >= arr[i][j - 1])) || (j == 0);
+    public static boolean isPeak(int i, int j, int[][] arr) {
+        int element = arr[i][j];
+        boolean right = ((j < arr[i].length - 1) && (element >= arr[i][j + 1])) || (j == arr[i].length - 1);
+        boolean left = ((j > 0) && (element >= arr[i][j - 1])) || (j == 0);
 
-		return right && left;
-	}
+        return right && left;
+    }
 
-	public static int getBestNeigbour(int i, int j, int[][] arr) {
-		int element = arr[i][j];
-		if ((j < arr[i].length - 1) && (element < arr[i][j + 1])) {
-			return j + 1;
-		}
-		if ((j > 0) && (element < arr[i][j - 1])) {
-			return j - 1;
-		}
+    public static int getBestNeighbour(int i, int j, int[][] arr) {
+        int element = arr[i][j];
+        if ((j < arr[i].length - 1) && (element < arr[i][j + 1])) {
+            return j + 1;
+        }
+        if ((j > 0) && (element < arr[i][j - 1])) {
+            return j - 1;
+        }
 
-		return -1;
-	}
+        return -1;
+    }
 
 }
