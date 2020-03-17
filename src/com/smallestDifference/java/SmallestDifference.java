5348b28122af36dc8afff44f75df45b8147b695c
Mahmoud Mossad <mahmoud.mossad@student.guc.edu.eg>
2020-03-17 12:51:41 +0200

    add the smallest difference algorithm

---
+++ src/com/smallestDifference/java/SmallestDifference.java
@@ -0,0 +1,45 @@
+package com.smallestDifference.java;
+
+import com.utilities.java.ArrayUtilities;
+
+import java.util.Arrays;
+
+public class SmallestDifference {
+
+
+    public static void main(String[] args) {
+        int[] arrA = {-1, 5, 10, 20, 28, 3};
+        int[] arrB = {26, 134, 135, 15, 157};
+        ArrayUtilities.printArray(smallestDifference(arrA, arrB));
+    }
+
+    public static int[] smallestDifference(int[] arrA, int[] arrB) {
+        Arrays.sort(arrA);
+        Arrays.sort(arrB);
+        int indexA = 0;
+        int indexB = 0;
+        double minimumDifference = Double.POSITIVE_INFINITY;
+        double currentDifference = Double.POSITIVE_INFINITY;
+        int[] smallestPair = new int[2];
+        while ((indexA < arrA.length) && (indexB < arrB.length)) {
+            int numA = arrA[indexA];
+            int numB = arrB[indexB];
+            currentDifference = Math.abs((numA - numB));
+            if (currentDifference < minimumDifference) {
+                minimumDifference = currentDifference;
+                smallestPair[0] = numA;
+                smallestPair[1] = numB;
+            }
+
+            if (numA < numB) {
+                indexA++;
+            } else if (numA > numB) {
+                indexB++;
+            } else {
+                break;
+            }
+
+        }
+        return smallestPair;
+    }
+}
