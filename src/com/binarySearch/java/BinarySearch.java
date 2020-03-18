6e0f163c69f3bcf007da68f818ac377f7ba80689
Mahmoud Mossad <mahmoud.mossad@student.guc.edu.eg>
2020-03-18 14:10:53 +0200

    add binary search iteratively algorithm

--- src/com/binarySearch/java/BinarySearch.java
+++ src/com/binarySearch/java/BinarySearch.java
@@ -4,7 +4,25 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 5, 9, 11, 11, 15, 20, 21, 21, 21, 80, 100, 500, 500, 5000};
-        System.out.println(binarySearchRecursive(arr, 22));
+        System.out.println(binarySearchIteratively(arr, 21));
+    }
+
+    public static int binarySearchIteratively(int[] arr, int target) {
+        int lowerBound = 0;
+        int upperBound = arr.length - 1;
+        int middle;
+        while (true) {
+            middle = (lowerBound + upperBound) / 2;
+            if (lowerBound > upperBound) {
+                return -1;
+            } else if (arr[middle] == target) {
+                return target;
+            } else if (arr[middle] > target) {
+                upperBound = middle - 1;
+            } else {
+                lowerBound = middle + 1;
+            }
+        }
     }
 
     public static int binarySearchRecursive(int[] arr, int target) {
