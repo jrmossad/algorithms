7f801977f30a63616c0918350132ece177369c0d
Mahmoud Mossad <mahmoud.mossad@student.guc.edu.eg>
2020-03-18 13:25:11 +0200

    add palindrome check reverse array algorithm

--- src/com/palindromeCheck/java/PalindromeCheck.java
+++ src/com/palindromeCheck/java/PalindromeCheck.java
@@ -3,15 +3,28 @@ package com.palindromeCheck.java;
 public class PalindromeCheck {
 
     public static void main(String[] args) {
-        System.out.println(palindromeCheckReverse("racecara"));
+        System.out.println(palindromeCheckReverseArray("racecara"));
     }
+
+//    public static boolean palindromeCheckRecursive(String string) {
+//
+//    }
 
     public static boolean palindromeCheckReverse(String string) {
         String newString = new StringBuilder(string).reverse().toString();
         return string.equals(newString);
     }
 
-    public static boolean palindromeCheck(String string) {
+    public static boolean palindromeCheckReverseArray(String string) {
+        char[] reverseString = new char[string.length()];
+        int j = 0;
+        for (int i = string.length() - 1; i >= 0; i--) {
+            reverseString[j++] = string.charAt(i);
+        }
+        return new String(reverseString).equals(string);
+    }
+
+    public static boolean palindromeCheckOptimized(String string) {
         int i = 0;
         int j = string.length() - 1;
         if (j == -1) {
