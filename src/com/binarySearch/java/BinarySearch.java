dbe4cb8ceec9ee61b03aff3e9f489db75488c48e
Mahmoud Mossad <mahmoud.mossad@student.guc.edu.eg>
2020-03-18 14:05:25 +0200

    add binary search algorithm

---
+++ src/com/binarySearch/java/BinarySearch.java
@@ -0,0 +1,28 @@
+package com.binarySearch.java;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 9, 11, 11, 15, 20, 21, 21, 21, 80, 100, 500, 500, 5000};
+        System.out.println(binarySearchRecursive(arr, 22));
+    }
+
+    public static int binarySearchRecursive(int[] arr, int target) {
+        return binarySearchRecursiveHelper(arr, target, 0, arr.length - 1);
+    }
+
+    public static int binarySearchRecursiveHelper(int[] arr, int target, int lowerBound, int upperBound) {
+        if (lowerBound > upperBound) {
+            return -1;
+        }
+        int middle = (lowerBound + upperBound) / 2;
+        if (arr[middle] == target) {
+            return target;
+        }
+        if (arr[middle] > target) {
+            return binarySearchRecursiveHelper(arr, target, lowerBound, middle - 1);
+        }
+        return binarySearchRecursiveHelper(arr, target, middle + 1, upperBound);
+    }
+
+}
