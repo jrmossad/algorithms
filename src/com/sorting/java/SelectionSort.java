7d34564acab0d7466d01755f6a54d40cd7b8fd55
Mahmoud Mossad <mahmoud.mossad@student.guc.edu.eg>
2020-03-18 11:48:45 +0200

    add selection sort (minimum) algorithm

--- src/com/sorting/java/SelectionSort.java
+++ src/com/sorting/java/SelectionSort.java
@@ -5,11 +5,11 @@ import com.utilities.java.ArrayUtilities;
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int[] arr = {1, 4, 3, 5};
-        ArrayUtilities.printArray(selectionSort(arr));
+        int[] arr = {8, 1, 4, 3, 5, 2, 7};
+        ArrayUtilities.printArray(selectionSortMin(arr));
     }
 
-    public static int[] selectionSort(int[] arr) {
+    public static int[] selectionSortMax(int[] arr) {
         int maximum;
         int temp;
         for (int i = 0; i < arr.length; i++) {
@@ -27,5 +27,20 @@ public class SelectionSort {
         return arr;
     }
 
-
+    public static int[] selectionSortMin(int[] arr) {
+        int minimum;
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            minimum = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minimum]) {
+                    minimum = j;
+                }
+            }
+            temp = arr[minimum];
+            arr[minimum] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
 }
