f780b9130cafe84006c2e2d5d096e7130c79046c
jrmossad <jrmossad@gmail.com>
2020-04-11 18:18:07 +0200

    add max heap

--- src/com/heap/java/MaxHeap.java
+++ src/com/heap/java/MaxHeap.java
@@ -5,104 +5,87 @@ import java.util.*;
 
 public class MaxHeap {
 
-    public Node root;
-    //Till I implement my own linked list and queue
-    private Queue<Node> nodesQueue;
+    private ArrayList<Integer> maxHeap;
 
-//    public MaxHeap(int[] arr) {
-//        nodesQueue = new LinkedList<>();
-//        root = null;
-//        initialize(arr);
-//    }
+    public MaxHeap(ArrayList<Integer> arr) {
+        maxHeap = arr;
+        buildHeap(maxHeap);
+    }
 
-//    public void initialize(int[] arr) {
-//        for (int i = 0; i < arr.length; i++) {
-//            System.out.println(i);
-//            Node current = new Node(arr[i]);
-//            nodesQueue.add(current);
-//            if (root == null) {
-//                root = current;
-//            } else if (nodesQueue.peek().leftChild == null) {
-//                System.out.println("hi");
-//                if (nodesQueue.peek().data > current.data) {
-//                    current.parent = nodesQueue.peek();
-//                    nodesQueue.peek().leftChild = current;
-//                } else {
-//                    Node previous = nodesQueue.peek();
-//                    while (previous != null && previous.data < current.data) {
-//                        current.leftChild = previous;
-//                        Node swap = current.rightChild;
-//                        current.rightChild = previous.rightChild;
-//                        previous.rightChild = swap;
-//                        if (previous.parent != null) {
-//                            previous.parent.leftChild = current;
-//                            previous.parent = previous.parent.leftChild;
-//                        } else {
-//                            root = current;
-//                        }
-//                        previous = previous.parent;
-//
-//                    }
-//                    Queue<Node> nodes = new LinkedList<>();
-////                    Node x = ;
-//
-//                    nodes.add(nodesQueue.remove());
-//
-//                    System.out.println(root.data);
-//                    nodesQueue = nodes;
-//                }
-//            } else if (nodesQueue.peek().rightChild == null) {
-//                if (nodesQueue.peek().data > current.data) {
-//                    current.parent = nodesQueue.peek();
-//                    nodesQueue.peek().rightChild = current;
-//
-//                } else {
-//                    Node previous = nodesQueue.peek();
-//                    while (previous.data < current.data) {
-//                        current.rightChild = previous;
-//                        Node swap = current.leftChild;
-//
-//                        current.leftChild = previous.leftChild;
-//                        previous.leftChild = swap;
-//                        if (previous.parent != null) {
-//                            previous.parent.rightChild = current;
-//                        } else {
-//                            root = current;
-//                        }
-//                        previous.parent = previous.parent.rightChild;
-//                        previous = previous.parent;
-//                    }
-//                    Queue<Node> nodes = new LinkedList<>();
-//                    this.printLeafNodes(this.root);
-//                }
-//                nodesQueue.remove();
-//            }
-//        }
-//    }
-//
-//    public void printLeafNodes(Node current) {
-//        if (current == null) {
-//            System.out.println();
-//            return;
-//        }
-//        if (current.leftChild == null && current.rightChild == null) {
-//            System.out.printf("%d ", current.data);
-//        }
-//        printLeafNodes(current.leftChild);
-//        printLeafNodes(current.rightChild);
-//    }
-//
-//    public static void main(String[] args) {
-//        int[] x = new int[]{1, 12, 13, 10, 11, 9, 8};
-//        MaxHeap arr = new MaxHeap(x);
-//        System.out.println(arr.root.data);
-//        System.out.println(arr.root.leftChild.data);
-//        System.out.println(arr.root.rightChild.data);
-//        System.out.println(arr.root.leftChild.leftChild.data);
-//        System.out.println(arr.root.leftChild.rightChild.data);
-//        System.out.println(arr.root.rightChild.leftChild.data);
-//        System.out.println(arr.root.rightChild.rightChild.data);
-//
-//    }
+    private ArrayList<Integer> buildHeap(ArrayList<Integer> arr) {
+        int firstParentIndex = (arr.size() - 2) / 2;
+        for (int i = firstParentIndex; i >= 0; i--) {
+            shiftDown(i, arr.size() - 1);
+        }
+        return arr;
+    }
 
+    public void insert(int value) {
+        maxHeap.add(value);
+        shiftUp(maxHeap.size() - 1);
+    }
+
+    public int delete() {
+        if (maxHeap.size() < 0) {
+            return -1;
+        }
+        swap(0, maxHeap.size() - 1);
+        int value = maxHeap.remove(maxHeap.size() - 1);
+        shiftDown(0, maxHeap.size() - 1);
+        return value;
+    }
+
+    private void shiftDown(int currentIndex, int endIndex) {
+        int childOneIndex = 2 * currentIndex + 1;
+        int indexToSwap;
+        while (childOneIndex <= endIndex) {
+            int childTwoIndex = 2 * currentIndex + 2 <= endIndex ? 2 * currentIndex + 2 : -1;
+            if (childTwoIndex != -1 && maxHeap.get(childTwoIndex) > maxHeap.get(childOneIndex)) {
+                indexToSwap = childTwoIndex;
+            } else {
+                indexToSwap = childOneIndex;
+            }
+            if (maxHeap.get(indexToSwap) > maxHeap.get(currentIndex)) {
+                swap(indexToSwap, currentIndex);
+                currentIndex = indexToSwap;
+                childOneIndex = 2 * currentIndex + 1;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void shiftUp(int currentIndex) {
+        int parentIndex = (currentIndex - 1) / 2;
+        while (currentIndex > 0 && maxHeap.get(currentIndex) > maxHeap.get(parentIndex)) {
+            swap(currentIndex, parentIndex);
+            currentIndex = parentIndex;
+            parentIndex = (currentIndex - 1) / 2;
+        }
+    }
+
+    public int peek() {
+        return maxHeap.size() > 0 ? maxHeap.get(0) : -1;
+    }
+
+    private void swap(int i, int j) {
+        Collections.swap(maxHeap, i, j);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(30);
+        arr.add(102);
+        arr.add(23);
+        arr.add(17);
+        arr.add(18);
+        arr.add(9);
+        arr.add(44);
+        arr.add(12);
+        MaxHeap maxHeap = new MaxHeap(arr);
+        System.out.println(maxHeap.peek());
+        for (int value : maxHeap.maxHeap) {
+            System.out.print(value + " ");
+        }
+    }
 }
