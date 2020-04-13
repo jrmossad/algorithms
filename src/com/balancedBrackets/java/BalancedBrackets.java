c3144ec38618631e853371db0460fa61671e91ed
jrmossad <jrmossad@gmail.com>
2020-04-13 14:12:23 +0200

    add balanced brackets clean algorithm

--- src/com/balancedBrackets/java/BalancedBrackets.java
+++ src/com/balancedBrackets/java/BalancedBrackets.java
@@ -1,14 +1,15 @@
 package com.balancedBrackets.java;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class BalancedBrackets {
 
     public static void main(String[] args) {
-        String str = "({[]}))";
-        System.out.println(balancedBrackets(str));
+        String str = "({[]})";
+        System.out.println(balancedBracketsClean(str));
     }
- 
+
     public static boolean balancedBrackets(String str) {
         Stack<Character> brackets = new Stack<>();
         for (int i = 0; i < str.length(); i++) {
@@ -40,6 +41,29 @@ public class BalancedBrackets {
                         brackets.pop();
                     }
                     break;
+            }
+        }
+        return brackets.empty();
+    }
+
+    public static boolean balancedBracketsClean(String str) {
+        Stack<Character> brackets = new Stack<>();
+        String openingBrackets = "({[";
+        String closingBrackets = ")}]";
+        HashMap<Character, Character> matchingBrackets = new HashMap<>();
+        matchingBrackets.put(')', '(');
+        matchingBrackets.put('}', '{');
+        matchingBrackets.put(']', '[');
+        for (int i = 0; i < str.length(); i++) {
+            char current = str.charAt(i);
+            if (openingBrackets.indexOf(current) != -1) {
+                brackets.push(current);
+            } else if ((closingBrackets.indexOf(current) != -1)) {
+                if (brackets.empty() || brackets.peek() != matchingBrackets.get(current)) {
+                    return false;
+                } else {
+                    brackets.pop();
+                }
             }
         }
         return brackets.empty();
