51b7296712d01219b2a40d085dd69fbe8e28dcba
jrmossad <jrmossad@gmail.com>
2020-04-15 18:14:09 +0200

    add stack logic by array list

---
+++ src/com/utilities/stack/java/Stack.java
@@ -0,0 +1,35 @@
+package com.utilities.stack.java;
+
+import java.util.ArrayList;
+
+public class Stack<T> {
+
+    private ArrayList<T> stack;
+
+
+    public Stack() {
+        stack = new ArrayList<>();
+    }
+
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    public T pop() {
+        if (stack.isEmpty()) {
+            return null;
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    public T peak() {
+        if (stack.isEmpty()) {
+            return null;
+        }
+        return stack.get(stack.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
